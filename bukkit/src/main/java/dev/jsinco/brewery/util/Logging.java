008d17365e4cfdf9896d34ca0e010b4ba8fb17bf
Nadwey <nadweyyy@gmail.com>
2024-11-05 14:43:46 +0100

    Improve Logging class

--- bukkit/src/main/java/dev/jsinco/brewery/util/Logging.java
+++ bukkit/src/main/java/dev/jsinco/brewery/util/Logging.java
@@ -5,17 +5,17 @@ import org.bukkit.Bukkit;
 
 public final class Logging {
 
-    public static void log(String m) {
+    public static void info(String m) {
         Bukkit.getConsoleSender().sendMessage(m);
     }
 
-    public static void debugLog(String m) {
+    public static void debug(String m) {
         if (TheBrewingProject.getInstance().getConfigManager().getConfig().verboseLogging) {
             Bukkit.getConsoleSender().sendMessage(m);
         }
     }
 
-    public static void logError(String m, Throwable throwable) {
+    public static void error(String m, Throwable throwable) {
         Bukkit.getConsoleSender().sendMessage(m);
     }
 }
